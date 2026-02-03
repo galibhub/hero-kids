c85128bb3cac3dd616ae1f42a72c318030530448
Ibrahim Ahmed Galib <galib@Mac-mini.local>
2026-02-03 18:59:04 +0600

    Add hero banner section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Banner from '../components/home/Banner';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <button className="btn btn-primary">test</button>
+    <div className="">
+     <section>
+      <Banner></Banner>
+     </section>
     </div>
   );
 }
